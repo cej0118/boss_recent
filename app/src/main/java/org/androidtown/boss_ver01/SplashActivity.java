92ef392858119e1c740f6666f2f2863d02a36474
hj <hjhj>
2018-03-29 02:03:57 +0900

    프로젝트 넣기

---
+++ app/src/main/java/org/androidtown/boss_ver01/SplashActivity.java
@@ -0,0 +1,15 @@
+package org.androidtown.boss_ver01;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this,SettingActivity.class));
+        finish();
+    }
+}
