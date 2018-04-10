b3829303ae97b1b4a0ecedd9795e7d250dafe6e2
hj <hjhj>
2018-04-10 21:02:17 +0900

    블루투스

---
+++ app/src/main/java/org/androidtown/boss_ver01/BluetoothService.java
@@ -0,0 +1,9 @@
+package org.androidtown.boss_ver01;
+
+/**
+ * Created by papil on 2018-04-10.
+ */
+
+public class BluetoothService {
+    // Debugging
+    // private static final String TAG = "BluetoothService"; private BluetoothAdapter btAdapter; private Activity mActivity; private Handler mHandler; // Constructors public BluetoothService(Activity ac, Handler h) { mActivity = ac; mHandler = h; // BluetoothAdapter 얻기 btAdapter = BluetoothAdapter.getDefaultAdapter(); }
